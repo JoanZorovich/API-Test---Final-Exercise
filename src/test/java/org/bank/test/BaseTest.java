474f16b14293add33272e04d31bf024dffd9de22
JoanZorovich <joanzorovichg@gmail.com>
2022-11-26 20:08:06 -0500

    add java doc

--- src/test/java/org/bank/test/BaseTest.java
+++ src/test/java/org/bank/test/BaseTest.java
@@ -12,6 +12,10 @@ import com.github.javafaker.Faker;
 
 import java.util.*;
 
+/**
+ * Base class for all API tests
+ * @author joan.zorovich
+ */
 
 public class BaseTest {
 
@@ -20,33 +24,60 @@ public class BaseTest {
 
     private int userAmount = 10;
 
+    /**
+     * Set up of before and after tests
+     */
 
     @BeforeTest
+    /**
+     * Set up the Base URI, Base Path and Content Type before every test
+     * @author joan.zorovich
+     */
     public void setup() {
-        //Test Setup
-        RestAssuredUtil.setBaseURI(); //Setup Base URI
-        RestAssuredUtil.setBasePath("users"); //Setup Base Path
-        RestAssuredUtil.setContentType(ContentType.JSON); //Setup Content Type
+        RestAssuredUtil.setBaseURI();
+        RestAssuredUtil.setBasePath("users");
+        RestAssuredUtil.setContentType(ContentType.JSON);
     }
 
     @AfterTest
     public void afterTest() {
-        //Reset Values
+        /**
+         * Reset the Base URI and Base Path and after every test
+         * @author joan.zorovich
+         */
         RestAssuredUtil.resetBaseURI();
         RestAssuredUtil.resetBasePath();
     }
 
+    /**
+     *Check the status code response
+     * @author joan.zorovich
+     * @return integer with the status code
+     *
+     */
     protected int checkGetStatus(){
         res = RestAssuredUtil.getResponse();
         return res.getStatusCode();
     }
 
+    /**
+     * Get all users of the end point
+     * @author joan.zorovich
+     * @return a list with all users
+     *
+     */
     protected List<User> getAllUsers(){
         res = RestAssuredUtil.getResponse();
         jp = RestAssuredUtil.getJsonPath(res);
         return jp.getList("", User.class);
     }
 
+    /**
+     * Delete each user from the end point
+     * @author joan.zorovich
+     * @return true if the end point is empty, otherwise false.
+     *
+     */
     protected boolean emptyTheEndpoint(){
         List<User> users = getAllUsers();
         users.forEach(user -> {
@@ -55,7 +86,12 @@ public class BaseTest {
         return getAllUsers().size()==0;
     }
 
-
+    /**
+     *Create users using fake information
+     * @author joan.zorovich
+     * @param usersAmount: number of users to create
+     * @return a list of fake users created
+     */
     protected ArrayList<User> createFakeUsers(int usersAmount){
         ArrayList<User> fakeUsers= new ArrayList<>();
         Faker fake = Faker.instance(Locale.forLanguageTag("en-US"));
@@ -76,7 +112,11 @@ public class BaseTest {
         return fakeUsers;
     }
 
-
+    /**
+     * create the user in the end point only if his/her email is unique
+     * @author joan.zorovich
+     * @return true if all users were successfully created, otherwise false.
+     */
     protected boolean postUsers(){
         List<String> emailAccounts = new ArrayList<>();
         List<Integer> userPostStatus = new  ArrayList<>();
@@ -90,6 +130,10 @@ public class BaseTest {
         return !userPostStatus.contains(400);
     }
 
+    /**
+     * @author joan.zorovich
+     * @return if no email in the endpoint is repeated, otherwise false.
+     */
     protected boolean checkDuplicateUsersEmail(){
         List<String> emailAccounts = new ArrayList<>();
         List<User> users = getAllUsers();
@@ -101,7 +145,11 @@ public class BaseTest {
         return emailAccounts.size() == users.size();
     }
 
-
+    /**
+     * Create a random index in order to pick a ransom user
+     * @author joan.zorovich
+     * @return a random user of the end point
+     */
     protected User pickRandomUser(){
         List<User> users = getAllUsers();
         Random randomIndex = new Random();
@@ -109,17 +157,34 @@ public class BaseTest {
         return users.get(index);
     }
 
+    /**
+     * Create a fake number using javaFaker
+     * @author joan.zorovich
+     * @return a fake account number
+     */
     protected int randomAccountNumber(){
         Faker fake = Faker.instance(Locale.forLanguageTag("en-US"));
         return fake.number().numberBetween(1,999999);
     }
 
+    /**
+     * Update the account number of a user that is already created in the endpoint
+     * @author joan.zorovich
+     * @param randomUser: user randomly obtained from endpoint
+     * @param newAccountNumber: fake number obtained from javaFaker
+     */
     protected void updateAccountNumber(User randomUser, int newAccountNumber){
         Map<String, Integer> user = new HashMap<>();
         user.put("accountNumber", newAccountNumber);
         res = RestAssuredUtil.putRequest(user,randomUser);
     }
 
+    /**
+     * Searches for the user whose account number was modified and compares
+     * whether the current account number is equal to the new account number
+     * @author joan.zorovich
+     * @return true if the new account number is actually present in the chosen user, otherwise false.
+     */
     protected boolean isAnUpdatedAccountNumber(){
         User randomUser= pickRandomUser();
         int newAccountNumber = randomAccountNumber();
